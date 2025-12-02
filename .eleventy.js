ac51bb5c373445357e98e9997dd97765277f4329
fjorheimar-maker <fjorheimar@gmail.com>
2025-12-02 19:41:26 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
 module.exports = function(eleventyConfig) {
   // Afrita static skrár (CSS, JS, myndir)
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/images");
+  // Nota object syntax til að tryggja réttar slóðir
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   
   // Afrita manifest og service worker fyrir PWA
-  eleventyConfig.addPassthroughCopy("src/manifest.json");
-  eleventyConfig.addPassthroughCopy("src/sw.js");
+  eleventyConfig.addPassthroughCopy({ "src/manifest.json": "manifest.json" });
+  eleventyConfig.addPassthroughCopy({ "src/sw.js": "service-worker.js" });
   
   return {
     dir: {
